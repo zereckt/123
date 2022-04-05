c1bd285b1f0615901554118350bdb41df0ddd0d8
zereckt <43085576+zereckt@users.noreply.github.com>
2022-04-05 02:53:19 +0000

    Deploying to gh-pages from @ zereckt/123@cde46e37622cdeb87cbd3310d8327cb12c8a293b 🚀

---
+++ _next/static/ENJ_BK4qcnss8vvTlEbWt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-efb890dc0c4a5d33.js"],"/_error":["static/chunks/pages/_error-e526a695839b5a36.js"],"/about":["static/chunks/pages/about-2665082ae1611364.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
